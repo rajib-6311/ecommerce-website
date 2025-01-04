27447e448759ff9a76baeaabe7662aa42198ae35
rajibhossen <mhossen6311@gmail.com>
2025-01-04 16:10:14 +0600

    Search bar update

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,17 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 import PropTypes from 'prop-types';
 
 export const ShopContext = createContext();
 
+
 const ShopContextProvider = (props) => {
   const currency = '$';
   const delivery_free = 10;  
+
+  const [search , setSearch] = useState('');
+  const [showSearch , setShowSearch] = useState(false);  
 
   const value = {
     products,
     currency,
     delivery_free,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return (
