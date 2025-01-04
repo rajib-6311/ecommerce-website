142516d908725148bd7ead068812a2d936072d31
rajibhossen <mhossen6311@gmail.com>
2025-01-04 13:02:21 +0600

    Sort by high to low and low to high

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -9,7 +9,8 @@ const Collection = () => {
     const [showFilter, setShowFilter] = useState(false);
     const [filterProducts, setFilterProducts] = useState([]);
     const [category , setCategory] = useState([]);
-    const [subCategory, setSubCategory] = useState([]);   
+    const [subCategory, setSubCategory] = useState([]); 
+    const [sortType, setSortType] = useState('relevant')  
     
     const toggleCategory = (e) => {
         if(category.includes(e.target.value)){
@@ -41,15 +42,39 @@ const Collection = () => {
       setFilterProducts(productsCopy)
     }
 
-    useEffect(()=>{
-        applyFilter();
-    },[category, subCategory])
+    // sort products by price
+    // filer Product Copy(fpCopy)
+    const sortProduct = () => {
+      let fpCopy = filterProducts.slice();
+      
+      switch(sortType) {
+        case 'High-Low':
+          setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+          break;
+
+        case 'Low-High':
+          setFilterProducts(fpCopy.sort((a,b) => (b.price - a.price)));
+          break;
+
+        default:
+          applyFilter();
+          break;  
+}
+    }
 
 
     useEffect(()=>{
         setFilterProducts(products)
     },[products])
-    
+
+    useEffect(()=>{
+      applyFilter();
+    },[category, subCategory])
+
+    useEffect(()=>{
+      sortProduct();
+    },[sortType])
+      
     return (
         <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
         {/* Filter Options   */}
@@ -93,7 +118,7 @@ const Collection = () => {
             <div className="flex justify-between text-base sm:text-2xl mb-4">
             <Title text1={'ALL'} text2={'COLLECTION'}/>
             {/* product sort  */}
-            <select className="border-2 border-gray-300 text-sm px-2 ">
+            <select onChange={(e)=>setSortType(e.target.value)} className="border-2 border-gray-300 text-sm px-2 ">
             <option value="relevant">Sort by: relevant</option>
             <option value="Low-High">Sort by: High to Low</option>
             <option value="High-Low">Sort by: Low to High</option>
