23612ca14d889134fef5c92a63eb83d493511f0b
rajibhossen <mhossen6311@gmail.com>
2025-01-09 00:44:39 +0600

    Search product update

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -17,6 +17,7 @@ const SearchBar = () => {
         setVisible(false);
        }
     },[location])
+    
     return showSearch  && visible ? (
         <div className="border-t border-b bg-gray-50 text-center">
         <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-/2">
